b6438d2615ceb3e8c76a0c01329cd6e64a82fe8a
Constantine Mars <constantine.mars@gmail.com>
2015-02-27 13:33:21 +0200

    map example

--- app/src/main/java/c/mars/rxjavasample/MainActivity.java
+++ app/src/main/java/c/mars/rxjavasample/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RxSample.run();
+//        RxSample.runSimpleWithLambdas();
+        RxSample.runMapSample();
     }
 
 
--- app/src/main/java/c/mars/rxjavasample/RxSample.java
+++ app/src/main/java/c/mars/rxjavasample/RxSample.java
@@ -3,6 +3,7 @@ package c.mars.rxjavasample;
 import android.util.Log;
 
 import rx.Observable;
+import rx.Subscriber;
 import rx.functions.Action1;
 
 /**
@@ -11,7 +12,41 @@ import rx.functions.Action1;
 public class RxSample {
     private static final String TAG = RxSample.class.getSimpleName();
 
-    public static void run() {
+    public static void runComplete() {
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("test");
+                subscriber.onCompleted();
+            }
+        });
+
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG, "completed");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "error");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "next: "+s);
+            }
+        };
+    }
+
+    public static void runSimpleWithLambdas() {
         Observable.just("z","y","x").subscribe(s -> Log.d(TAG, "next:"+s));
+    }
+
+    public static void runMapSample() {
+        Observable.just("z","y","x")
+                .map(s -> s.hashCode())
+                .map(i -> "hash:"+i)
+                .subscribe(s -> Log.d(TAG, "next:"+s));
     }
 }
